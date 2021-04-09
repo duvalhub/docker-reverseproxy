25b7c5873e56e863e1f3e42866283d6bcaa3403b
Maxthod <maxthod@pm.me>
2021-04-09 00:45:45 -0400

    improvement/added-usage-to-release-script

--- release.sh
+++ release.sh
@@ -4,6 +4,17 @@ set -e
 declare -r version="${1:-latest}"
 declare -r image="duvalhub/nginx-companion:$version"
 
+while [[ $# -gt 0 ]]; do
+case "$1" in
+    -h|--help)
+        echo "$0 [version]"
+        echo "version       Optional. Version to build and release. Default to latest"
+        exit
+    ;;
+esac
+shift
+done
+
 if [[ "$version" != "latest" && ! -z "$(git status --porcelain)" ]]; then 
   echo "Working directory not clean. Commit before release"
   exit 1
