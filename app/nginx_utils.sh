05d3e1fe04afcc4b303f35f454d2fd09d5192bba
Maxthod <maxthod@pm.me>
2021-04-15 21:01:00 -0400

    "fix/nginx-start-on-upstream-unreachable"

--- app/nginx_utils.sh
+++ app/nginx_utils.sh
@@ -102,14 +102,17 @@ nginx_write_http_block() {
     local -i port="${params[2]:-80}"
     cat <<-EOF
 # BEGIN $name
-upstream $base_domain {
-    server $name:$port;
-}
+# upstream $base_domain {
+#     server $name:$port;
+# }
 server {
     server_name $dns;
     listen 80 ;
+    resolver 127.0.0.11 valid=30s;
     include /etc/nginx/vhost.d/default;
     location / {
+        set $upstream $name:$port;
+        proxy_pass http://$upstream;
         proxy_pass http://$base_domain;
     }
 }
@@ -125,9 +128,9 @@ nginx_write_https_block() {
     local -i port="${params[2]:-80}"
     cat <<-EOF
 # BEGIN $name
-upstream $base_domain {
-    server $name:$port;
-}
+# upstream $base_domain {
+#     server $name:$port;
+# }
 server {
     server_name $dns;
     listen 80 ;
@@ -140,6 +143,7 @@ server {
 server {
     server_name $dns;
     listen 443 ssl http2 ;
+    resolver 127.0.0.11 valid=30s;
     access_log /var/log/nginx/access.log vhost;
     ssl_session_timeout 5m;
     ssl_session_cache shared:SSL:50m;
@@ -147,7 +151,9 @@ server {
     ssl_certificate /etc/nginx/certs/$base_domain.crt;
     ssl_certificate_key /etc/nginx/certs/$base_domain.key;
     location / {
-        proxy_pass http://$base_domain;
+        set $upstream $name:$port;
+        proxy_pass http://$upstream;
+        # proxy_pass http://$base_domain;
     }
 }
 # END $name
